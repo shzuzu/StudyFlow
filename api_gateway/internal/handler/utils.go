08c04d157e47434cfe21b4ce23ce5d0a640f2207
Kandler3 <artchuk@icloud.com>
2025-04-23 12:46:01 +0300

    cache added

--- api_gateway/internal/handler/utils.go
+++ api_gateway/internal/handler/utils.go
@@ -13,6 +13,7 @@ import (
 	"google.golang.org/protobuf/proto"
 	"io"
 	"net/http"
+	"time"
 )
 
 var (
@@ -20,6 +21,12 @@ var (
 	BadRequestError    = errors.New("bad request")
 	WrongGrpcTypeError = errors.New("wrong grpc request type")
 )
+
+type Cache interface {
+	Get(ctx context.Context, key string) ([]byte, bool)
+	Set(ctx context.Context, key string, data []byte, ttl time.Duration)
+	Delete(ctx context.Context, key string)
+}
 
 func mapErr(err error) int {
 	if errors.Is(err, BadRequestError) {
@@ -117,6 +124,82 @@ func Handle[Req any, Resp any](
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(data)
+	}, nil
+}
+
+func HandleWithCache[Req any, Resp any](
+	method func(context.Context, *Req, ...grpc.CallOption) (*Resp, error),
+	reqParser func(context.Context, *http.Request, *Req) error,
+	parseBody bool,
+	cache Cache,
+	keyFunc func(r *http.Request) (string, error),
+	ttl time.Duration,
+) (http.HandlerFunc, error) {
+	if method == nil {
+		return nil, NilMethodError
+	}
+	if _, ok := any(new(Req)).(proto.Message); !ok {
+		return nil, WrongGrpcTypeError
+	}
+	if _, ok := any(new(Resp)).(proto.Message); !ok {
+		return nil, WrongGrpcTypeError
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := metadata.NewOutgoingContext(r.Context(), metadata.Pairs())
+		if id := r.Header.Get("X-User-Id"); id != "" {
+			ctx = metadata.AppendToOutgoingContext(ctx, "x-user-id", id)
+		}
+		if role := r.Header.Get("X-User-Role"); role != "" {
+			ctx = metadata.AppendToOutgoingContext(ctx, "x-user-role", role)
+		}
+
+		key, err := keyFunc(r)
+		if err == nil {
+			if data, ok := cache.Get(ctx, key); ok {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				w.Write(data)
+				return
+			}
+		}
+
+		grpcReq := new(Req)
+		if parseBody {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			if err := protojson.Unmarshal(body, any(grpcReq).(proto.Message)); err != nil {
+				w.WriteHeader(mapErr(err))
+				return
+			}
+		}
+		if reqParser != nil {
+			if err := reqParser(ctx, r, grpcReq); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+
+		grpcResp, err := method(ctx, grpcReq)
+		if err != nil {
+			w.WriteHeader(mapErr(err))
+			return
+		}
+
+		data, err := protojson.Marshal(any(grpcResp).(proto.Message))
+		if err != nil {
+			w.WriteHeader(mapErr(err))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+
+		if key != "" {
+			cache.Set(ctx, key, data, ttl)
+		}
 	}, nil
 }
 
